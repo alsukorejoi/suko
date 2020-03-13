d005f54f05005b489f606c70e9ac64c635588b71
alsukorejoi <34817089+alsukorejoi@users.noreply.github.com>
2020-03-14 05:00:31 +0700

    Update DbXZPMBQ.js

--- DbXZPMBQ.js
+++ DbXZPMBQ.js
@@ -16,7 +16,7 @@ var jwDefaults = {
   "flashplayer": "//ssl.p.jwpcdn.com/player/v/7.12.13/jwplayer.flash.swf",
   "height": "100%",
   "hlshtml": true,
-  "key": "VbO5SGE6w1c/k1D5sh6/yDF2W3OGbaWzZFMwJ6aaiY44NntoZIVndg==",
+  "key": "tEXCUqak1ripqN0gCHnciQHinPXgHJWzufkRRESFM4mjrZs0StnMwQ==",
   "mute": true,
   "ph": 1,
   "pid": "DbXZPMBQ",
